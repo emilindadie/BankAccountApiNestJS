b8a02cd069ae0404c3033577e151f24f2ba7cd90
Emilin DADIE <dadie.emilin@gmail.com>
2020-05-07 22:45:22 +0200

    fixed heroku

--- src/Config/dbConfig.ts
+++ src/Config/dbConfig.ts
@@ -6,11 +6,11 @@ export class DbConfig implements TypeOrmOptionsFactory {
     createTypeOrmOptions(): TypeOrmModuleOptions {
         return {
             type: 'postgres',
-            host: process.env.DB_HOST,
+            host: process.env.HOST,
             port: Number(process.env.DB_PORT),
-            username: process.env.DB_USERNAME,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_DATABASE,
+            username: process.env.USERNAME,
+            password: process.env.PASSWORD,
+            database: process.env.DATABASE,
             entities: ["dist/**/*.entity{.ts,.js}"],
             synchronize: true,
             logging: true,
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 
+declare const module: any;
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.APP_PORT || 8080;
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  );
+  await app.listen(Number(port), '0.0.0.0');
+  Logger.log(`App listening on port ${port}!`)
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
